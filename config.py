65012a9ade869acfbb0259fc0e7e442c2e689615
TaHa <tahanasrollahii@gmail.com>
2026-07-06 14:23:44 +0330

    update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 class Settings(BaseSettings):
     FASTAPI_API_KEY: str = "default_secret_api_key_for_dev"
-    DATABASE_URL: str = "sqlite:///E:/ForNow/Programming/Advance/kelaasor_back/db.sqlite3"
+    DATABASE_URL: str = "sqlite:///path/to/kelaasor_back/db.sqlite3"
     REDIS_URL: str = "redis://localhost:6379/0"
     DJANGO_WEBHOOK_URL: str = "http://127.0.0.1:8000/api/webhooks/video-status/"
-    MEDIA_DIR: str = "E:/ForNow/Programming/Advance/kelaasor_back/media"
+    MEDIA_DIR: str = "E:/path/to/kelaasor_back/media"
     
     class Config:
         env_file = ".env"
